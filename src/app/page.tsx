d2d24bfae2575554acead297b8279dcf09ae4e0d
Jose Andres Garcia <Gorusuke123@gmail.com>
2024-03-22 19:32:38 -0500

    Update home component

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,21 +1,20 @@
 import Image from "next/image";
 import { getAnimeList } from "@/lib/data";
-import styles from "./page.module.css";
 import AnimeCard from "@/Components/AnimeCard";
 import { Anime } from "@/lib/interfaces";
+import LoadMore from "@/Components/LoadMore";
+import styles from "./page.module.css";
 
 export default async function Home() {
-  const animeList: Anime[] = await getAnimeList()
+  const animeList: Anime[] = await getAnimeList(1)
   
   return (
     <main className={styles.homeMain}>
       <h2 className={styles.homeTitle}>Explore Anime</h2>
       <section className={styles.animeContainer}>
-        {animeList.map(anime => <AnimeCard key={anime.id} anime={anime} /> )}
+        {animeList.map(anime => <AnimeCard key={`${anime.id}${anime.url}`} anime={anime} /> )}
       </section>
-      <div className={styles.spinner}>
-        <Image src="/spinner.svg" alt="loading" width={35} height={35}/>
-      </div>
+      <LoadMore />
     </main>
   );
 }
